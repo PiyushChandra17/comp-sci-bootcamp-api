0beb6deaf91eeb8bcfeca55f7613159d122312a7
PiyushChandra17 <chandra.piyush17@gmail.com>
2023-04-04 16:19:59 +0530

    added readme

---
+++ readme.md
@@ -0,0 +1,29 @@
+# CSBootcamp API
+
+> Backend API for CSBootcamp application, which is a bootcamp directory website
+
+## Usage
+
+Rename "config/config.env.env" to "config/config.env" and update the values to your own accordingly
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Run App
+```
+# Run in dev mode
+npm run dev
+
+# Run in prod mode
+npm start
+```
+
+- Version: 1.0.0
+- License: MIT
+
+
+
+
---
+++ routes/courses.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router({ mergeParams: true })
+const { protect,authorize } = require('../middleware/auth')
+
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse} = require('../controllers/courses')
+
+const Course = require('../models/Course')
+const advanceResults = require('../middleware/advanceResults')
+
+router.route('/').get(advanceResults(Course, { path: 'bootcamp', select: 'name description'}),getCourses).post(protect,authorize('publisher', 'admin'),addCourse)
+router.route('/:id').get(getCourse).put(protect,authorize('publisher', 'admin'),updateCourse).delete(protect,authorize('publisher', 'admin'),deleteCourse)
+
+module.exports = router
